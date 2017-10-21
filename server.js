bcdf3f930c99e6ca0ebecb7db573039eb44724fa
freethewhat <mcsutton@outlook.com>
2017-10-21 16:24:35 -0600

    Adding routes

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 
-const Recipe = require('./models/recipes');
+const Recipe = require('./models/recipe');
+const Ingredient = require('./models/ingredient');
 
 mongoose.connect("mongodb://localhost:27017/cakes", {useMongoClient: true});
 
@@ -18,11 +19,11 @@ router.use(function(req, res, next) {
   next();
 });
 
-router.route("/recipes")
+router.route("/recipe")
   .post(function(req, res) {
     var recipe = new Recipe();
     recipe.name = req.body.name;
-    recipe.ingredients = ['flour', 'egg', 'sugar'];
+    recipe.ingredients;
     recipe.directions = "Put the darn thing togetha and put in the over";
     recipe.image = "invalid";
 
@@ -43,7 +44,7 @@ router.route("/recipes")
     });
   });
 
-router.route('/recipes/:recipe_id')
+router.route('/recipe/:recipe_id')
   .get(function(req, res) {
     Recipe.findById(req.params.recipe_id, function(err, recipe) {
       if(err)
@@ -70,6 +71,78 @@ router.route('/recipes/:recipe_id')
       })
     })
   })
+  .delete(function(req, res) {
+    Recipe.remove({
+      _id: req.params.recipe_id
+    }, function(err, recipe) {
+      if(err)
+        res.send(err);
+
+      res.json({message: 'successfully deleted'});
+    });
+  });
+
+router.route('/ingredient')
+  .post(function(req, res) {
+    var ingredient = new Ingredient();
+
+    ingredient.name = "Egg";
+    ingredient.allergens = [Math.random() * 10];
+    ingredient.calories = Math.random() * 1000;
+
+    ingredient.save(function(err) {
+      if(err)
+        res.send(err);
+
+      res.json({message: "Ingredient created!"});
+    });
+  })
+  .get(function(req, res) {
+    Ingredient.find(function(err, ingredients) {
+      if (err)
+        res.send(err);
+
+      res.json(ingredients);
+    });
+  });
+
+router.route('/ingredient/:ingredient_id')
+  .get(function(req, res) {
+    Ingredient.findById(req.params.ingredient_id, function(err, ingredient) {
+      if(err)
+        res.send(err);
+
+      console.log(err)
+      res.json(ingredient);
+    });
+  })
+  .put(function(req, res) {
+    Ingredient.findById(req.params.ingredient_id, function(err, ingredient) {
+      if(err)
+        res.send(err);
+
+      ingredient.name = 'flour';
+      ingredient.allergens = ['peanut', 'gluten'];
+      ingredient.calories = Math.floor(Math.random() * 1000);
+
+      ingredient.save(function(err) {
+        if (err)
+          res.send(err)
+
+        res.json({ message: 'Ingredient Updated!'});
+      });
+    });
+  })
+  .delete(function(req, res) {
+    Ingredient.remove({
+      _id: req.params.ingredient_id
+    }, function(err, ingredient) {
+      if(err)
+        res.send(err);
+
+      res.json({message: 'successfully deleted'});
+    })
+  });
 app.use('/api',router)
 
 app.listen(port);
